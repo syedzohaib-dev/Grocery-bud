9b3bbcdcddecdb6883935625ddd37b6350286c35
syed zohaib <zohaibsyed914@gmail.com>
2024-06-14 17:09:03 +0500

    working in under construction

---
+++ app.js
@@ -0,0 +1,9 @@
+const submit = document.querySelector('.submit')
+const input = document.querySelector('.input')
+const ul = document.querySelector('ul')
+
+submit.addEventListener("click", () => {
+    console.log(input.value)
+    const li = document.createElement("li")
+    li.innerHTML = 
+})
